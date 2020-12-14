e9fed4ce850e9d73a5efc396526b898552cf66fc
Balázs Buri <burib@users.noreply.github.com>
2020-12-14 20:23:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = "~> 0.13.0"
   required_providers {
     aws = {
-      version = ">= 3.11.0"
       source  = "hashicorp/aws"
+      version = "~> 3.3.0"
+    }
+    template = {
+      source = "hashicorp/template"
     }
   }
+  backend "s3" {}
 }
